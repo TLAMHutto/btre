5caf1b7d5c60b73d10438fc8a6bb15429ef9e7fb
TLAMHutto <keatonhutto8@gmail.com>
2022-09-04 15:16:07 -0500

    listings

--- btre/urls.py
+++ btre/urls.py
@@ -6,4 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
+    path('listings/', include('listings.urls')),
 ]
